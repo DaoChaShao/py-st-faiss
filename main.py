a59221a4c9bdaec31b7c7381d949c8e96b7157cf
DaoChaShao <27882272@qq.com>
2025-02-23 00:38:05 +1300

    feat: add main.py to initialize application and set up page navigation

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/22 22:31
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   main.py
+# @Desc     :
+
+from utilis.layout import pages_setter
+
+
+def main() -> None:
+    """ streamlit run main.py """
+    pages_setter()
+
+
+if __name__ == "__main__":
+    main()
