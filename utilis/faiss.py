397e35ec3fece87914441d1b977e8af71efbf1b7
DaoChaShao <27882272@qq.com>
2025-02-23 00:37:42 +1300

    feat: add faiss.py with main function template

---
+++ utilis/faiss.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/22 22:32
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   faiss.py
+# @Desc     :   
+
+
+
+
+def main() -> None:
+    """ streamlit run main.py """
+
+
+
+if __name__ == "__main__":
+    main()
